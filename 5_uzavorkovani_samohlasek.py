b94223ac4ff8ed22c5b1d4d1faee4eb78ebbdb70
Eva Lemberková <lemberke@natur.cuni.cz>
2023-02-09 00:20:37 +0100

    pridany komentare

--- 5_uzavorkovani_samohlasek.py
+++ 5_uzavorkovani_samohlasek.py
@@ -1,3 +1,4 @@
+#vyjimka proti otevreni prazdneho nebo neexistujiciho souboru
 try:
     with open("5_vstupni_text.txt", encoding = "utf-8") as f:
         assert(len(f.readlines()) > 0)
@@ -10,17 +11,22 @@ except AssertionError:
     print("Ve vstupním souboru nejsou žádná data.")
     quit()
 
+#vytvoreni tridy "Character", ktera obsahuje staticky seznam samohlasek
 class Character:
     vowels = ["a", "á", "e", "ě", "é", "i", "í", "o", "ó", "u", "ů", "ú", "y", "ý"]
 
+#inicializace
     def __init__(self, character):
             self.__character = character
 
-    def isVowel(self): #metoda
+#metoda isVowel porovnava nacteny znak se seznamem samohlasek a rozhodne, jestli je dany znak samohlaska
+    def isVowel(self):
         if self.__character in Character.vowels:
             return True
-        
-with open("5_vystupni_text.txt", "w", encoding = "utf-8") as f:       
+
+#otevreni souboru "5_vystupni_text.txt" pro psani       
+with open("5_vystupni_text.txt", "w", encoding = "utf-8") as f:  
+#prochazeni jednotlivych znaku v souboru a pokud to jsou samohlasky, napisi se do souboru ohranicene zavorkami a pokud to jsou jine znaky, napisi se do souboru v nezmenenne podobe     
     for znak in text:
         ch = Character(znak.lower())
         if ch.isVowel() == True:
